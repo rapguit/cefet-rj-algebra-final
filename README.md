efb80f079f1c9428ca3fd80d420e170a7cfc715d
Gustavo Alexandre <gassantos@id.uff.br>
2018-06-02 02:52:18 -0300

    Updating README
    Update of the README with all commands and runtime parameters.

--- README.md
+++ README.md
@@ -1,11 +1,51 @@
-# cefet-rj-algebra-final
-##### Python 3.5
+# Trabalho Final de Algebra Linear Computacional
+##### Enviroment: Python 3.6
 
-Implementar métodos de redução de banda de matrizes
-http://www.sbmac.org.br/arquivos/notas/livro_75.pdf
+Implementar métodos de redução de banda de matrizes com base nas heurísticas apresentadas em
+http://www.sbmac.org.br/arquivos/notas/livro_75.pdf.
 
-Implementar qualquer um dos métodos do livro, e o gradiente conjugado.
+* Implementar uma das Heurísticas apresentadas no livro;
+* Implementar o Gradiente Conjugado com Técnica de Armazenamento de Linha (**CSR**);
+* Comparar a Heurística escolhida e o Gradiente Conjugado com relação ao tempo de processamento.
 
-Comparar os resultados do gradiente conjugado com e sem redução de banda em relação ao tempo de processamento.
+_OBS.: Usar matrizes grandes com aqueles esquemas de compactação. Para maiores detalhes, acesse este catálogo de matrizes https://sparse.tamu.edu._
 
-Usar matrizes grandes com aqueles esquemas de compactação.
+
+## Como utilizar
+
+Primeiramente, baixe o repostiório para a sua máquina.
+Após baixá-lo, o repositório **raiz** (_cefet-rj-algebra-final_) terá a seguinte estrutura:
+
+* Arquivos:
+    * README.md
+    * application.py
+* Diretórios: 
+    * datasets
+    * modules
+    * tests
+
+Em seguida, acesse o diretório **_datasets_** na sua máquina e verifique os arquivos de matrizes do tipo *.mtx. Esses arquivos serão carregados na etapa de execução.
+
+Um outro diretório importante a ser verificado é o **_modules_**, mais precisamente o arquivo contido neste diretório chamado _heuristic_cuthillmckee_. Esse arquivo será utilizado para rodar a heuristica utilizada neste trabalho para fins de comparação. Para executá-la:
+
+* **python** _**heuristic_cuthillmckee.py**_ [**symetric_mode**] [**filename**]
+
+#### Exemplo
+* **python** _**modules/heuristic_cuthillmckee.py**_ **True**    **datasets/bcsstk18.mtx**
+
+Para conhecer os parâmetros, execute **python** _**heuristic_cuthillmckee.py**_ **-h**
+
+**OBS.: É necessário ter o Python (3.6) instalado em sua máquina.**
+
+Por fim, para executar o **Gradiente Conjugado**, basta executar o arquivo _application.py_ da seguinte forma:
+
+* **python** _**application.py**_ **--maxit INT** **--err FLOAT** **FILENAME.mtx**
+
+#### Exemplo
+* **python** _**application.py**_ **--maxit 100** **--err 0.01** **datasets/input11948x11948.mtx**
+
+Para conhecer os parâmetros, execute **python** _**application.py**_ **-h**.
+
+Então "mãos à obra" e bons resultados.
+
+Abs =D
--- modules/heuristic_cuthillmckee.py
+++ modules/heuristic_cuthillmckee.py
@@ -1,19 +1,4 @@
 
-"""
-    Description
-    -----------
-    Solve a linear equation Ax = b with conjugate gradient method.
-    Parameters
-    ----------
-    A: 2d numpy.array of positive semi-definite (symmetric) matrix
-    b: 1d numpy.array
-    x: 1d numpy.array of initial point
-    Returns
-    -------
-    1d numpy.array x such that Ax = b
-"""
-
-# Coleção de matrizes esparsas: https://sparse.tamu.edu
 
 import argparse
 import numpy as np
@@ -24,37 +9,57 @@ from scipy.io.mmio import mmread
 from scipy.sparse import csr_matrix
 from scipy.sparse.csgraph import reverse_cuthill_mckee
 
+def heuristica_bandwidth(file, symetric):
+    """
+    Descrição
+    -----------
+    Reduz a largura de banda de uma Matriz grandes com a Heurística Reverse-Cuthill-Mckee.
+    Paramêtros
+    ----------
+    Symetric_mode: True (ou False), se a Matriz for simetrica (ou nao simetrica)\n
+    filename: O nome do arquivo da matriz (extensoes .mtx, .mtz.gz)
+    Retorno
+    -------
+    O tempo de execução da heurística de redução de largura de banda.
+    Para acessar a coleção de matrizes esparsas: https://sparse.tamu.edu
+    -------------------------------------------------------------------- 
+    """
 
-# Definição do argumentos de execução
-parser = argparse.ArgumentParser(description="Tempo de Execucao da Heuristica para Reducao de Largura de Banda")
-group = parser.add_mutually_exclusive_group()
-group.add_argument("-v", "--verbose", action="store_true")
-group.add_argument("-q", "--quiet", action="store_true")
-parser.add_argument("symetric_mode", type=bool, help=": True (ou False), se a Matriz for simetrica (ou nao simetrica)")
-parser.add_argument("filename", type=str, help=": O nome do arquivo da matriz (extensoes .mtx, .mtz.gz)")
-args = parser.parse_args()
+    # Carregando o arquivo de formao Matriz Market filename
+    matriz = mmread(file)
+    print("\nApresentando a Matriz:")
+    print("\tDimensao (M): ", matriz.shape)
+    print("\tElementos NONZERO: ", matriz.nnz)
+    print("\tArquivo da Matriz: ", file)
 
-if args.quiet:
-    print("Estao faltando argumentos")
-elif args.verbose:
-    print("A Reducao de Largura de Banda vai ser aplicada na Matriz (*.mtx) {} com forma SIMETRICA = {}.".format(args.filename, args.symetric_mode))
+    # Convertendo com CSR uma Matriz Esparsa
+    G_sparse = csr_matrix(matriz)
+
+    print("\nAplicando a Heuristica REVERSE-CUTHIL-MCKEE")
+    t1 = time.time()
+    print("\tInicio\t- ", date.datetime.fromtimestamp(t1))
+    reverse_cuthill_mckee(G_sparse, symetric)
+    t2 = time.time()
+    print("\tTermino\t- ", date.datetime.fromtimestamp(t2))
+    print("Tempo de Execucao da Heuristica REVERSE-CUTHIL-MCKEE: ", t2 - t1)
+    print("\n")
 
 
-# Carregando o arquivo de formao Matriz Market filename
-matriz = mmread(args.filename)
-print("\nApresentando a Matriz:")
-print("\tDimensao (M): ", matriz.shape)
-print("\tElementos NONZERO: ", matriz.nnz)
-print("\tArquivo da Matriz: ", args.filename)
+if __name__ == '__main__':
+    
+    # Definição do argumentos de execução
+    parser = argparse.ArgumentParser(description="Tempo de Execucao da Heuristica para Reducao de Largura de Banda")
+    group = parser.add_mutually_exclusive_group()
+    group.add_argument("-v", "--verboso", help="Execucao em tela", action="store_true")
+    group.add_argument("-q", "--silencioso", help="(Default) Execucao em background", action="store_true")
+    parser.add_argument("symetric_mode", type=bool, help=": True (ou False), se a Matriz for simetrica (ou nao)")
+    parser.add_argument("filename", type=str, help=": O nome do arquivo da matriz (extensoes .mtx, .mtz.gz)")
+    args = parser.parse_args()
 
-# Convertendo com CSR uma Matriz Esparsa
-G_sparse = csr_matrix(matriz)
+    if args.silencioso:
+        print("Estao faltando argumentos")
+    elif args.verboso:
+        print("A Reducao de Largura de Banda vai ser aplicada na Matriz (*.mtx) {} com forma SIMETRICA = {}.".format(args.filename, args.symetric_mode))
 
-print("\nAplicando a Heuristica REVERSE-CUTHIL-MCKEE")
-t1 = time.time()
-print("\tInicio\t- ", date.datetime.fromtimestamp(t1))
-heuristica = reverse_cuthill_mckee(G_sparse, args.symetric_mode)
-t2 = time.time()
-print("\tTermino\t- ", date.datetime.fromtimestamp(t2))
-print("Tempo de Execucao da Heuristica REVERSE-CUTHIL-MCKEE: ", t2 - t1)
-print("\n")
+    # Executa a heuristica
+    heuristica_bandwidth(args.filename, args.symetric_mode)
