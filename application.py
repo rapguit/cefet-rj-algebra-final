5f36a5af6c1ba62eff867aa7d3e8dd75890dc2ae
Gustavo Alexandre <gassantos@id.uff.br>
2018-06-03 22:12:54 -0300

    Adjustments for app execution with default parameters.

--- application.py
+++ application.py
@@ -1,17 +1,63 @@
-from modules.printer import print_stats
+import datetime as date
+import time as time
 
+from modules.printer import print_stats
 from modules.args_factory import create_parser
 from modules.csr_loader import load
 from modules.conjugated_gradient import ConjugatedGradient
+from modules.heuristic_cuthillmckee import heuristica_bandwidth
 
 if __name__ == '__main__':
     parser = create_parser()
     args = parser.parse_args()
 
-    data = load(args.mtx_file)
+    if args.maxit == None:
+        args.maxit = 100
+    elif args.err == None:
+        args.err = 0.01
+    elif args.filename == None:
+        print("Parametro requerido para a aplicacao.")
+    else:
+        print("\n============================ COMPARACAO DE DESEMPENHO: CG vs RCM ============================\n")
 
+    # Carregando o arquivo de matriz
+    data = load(args.filename)
+
+    print("\n-------------------------------------------------------------------------------------------------")
+    print(" [RELATORIO] - Descricao da Execucao")
+    print("-------------------------------------------------------------------------------------------------")
+    print("\n Aplicando o Metodo Iterativo GRADIENTE-CONJUGADO")
+    t1 = time.time()
+    print("\tInicio\t- ", date.datetime.fromtimestamp(t1))
+    
+    # Execução do Gradiente Conjugado
     gradient = ConjugatedGradient(args.maxit, args.err)
     gradient.calculate(data)
+    t2 = time.time()
 
-    print_stats(gradient)
+    tGC = t2 - t1
+    print("\tTermino\t- ", date.datetime.fromtimestamp(t2))
+    print(" Tempo de Execucao do Metodo Iterativo GRADIENTE-CONJUGADO: ", tGC)
+    print("\n")
 
+    # Execução da Heurística de Redução de Bandwidth RCM
+    tRCM = heuristica_bandwidth(args.filename, args.symetric_mode)
+
+    print("\n-------------------------------------------------------------------------------------------------")
+    print(" [RESULTADO] - Avaliacao do Tempo de Execucao")
+    print("-------------------------------------------------------------------------------------------------\n")
+
+    if(tGC < tRCM):
+        print(" GRADIENTE-CONJUGADO teve DESEMPENHO MELHOR que REVERSE-CUTHIL-MCKEE")
+    elif (tGC > tRCM):
+        print(" REVERSE-CUTHIL-MCKEE teve DESEMPENHO MELHOR que GRADIENTE-CONJUGADO")
+    else:
+        print(" REVERSE-CUTHIL-MCKEE e GRADIENTE-CONJUGADO tiveram DESEMPENHO SEMELHANTE")
+
+    print("-------------------------------------------------------------------------------------------------")
+
+    # Auxílio por help
+    print("\n[Ajuda] Para conhecer os parâmetros, execute: 'python application.py -h'")
+    print("[Exemplo] Para execucao padrao, execute: 'python application.py datasets/<FILE>.mtx' \n")
+
+        
--- modules/heuristic_cuthillmckee.py
+++ modules/heuristic_cuthillmckee.py
@@ -1,6 +1,5 @@
 
 
-import argparse
 import numpy as np
 import datetime as date
 import time as time
@@ -9,11 +8,15 @@ from scipy.io.mmio import mmread
 from scipy.sparse import csr_matrix
 from scipy.sparse.csgraph import reverse_cuthill_mckee
 
+# Constants
+NONSYMETRIC = "==> Execução menos otimizada. OBS.: Para otimizar, acrescente 'True' na execucao)"
+YESSYMETRIC = "==> Execução mais otimizada ;)"
+
 def heuristica_bandwidth(file, symetric):
     """
     Descrição
     -----------
-    Reduz a largura de banda de uma Matriz grandes com a Heurística Reverse-Cuthill-Mckee.
+    Reduz a largura de banda de uma Matriz grandes com a Heurística RCM (Reverse-Cuthill-Mckee).
     Paramêtros
     ----------
     Symetric_mode: True (ou False), se a Matriz for simetrica (ou nao simetrica)\n
@@ -21,45 +24,43 @@ def heuristica_bandwidth(file, symetric):
     Retorno
     -------
     O tempo de execução da heurística de redução de largura de banda.
-    Para acessar a coleção de matrizes esparsas: https://sparse.tamu.edu
-    -------------------------------------------------------------------- 
+
+    Para maiores detalhes sobre a heurística desta classe:
+    --------------------------------------------------------------
+    Gonzaga de Oliveira, Sanderson L., "INTRODUÇÃO A HEURÍSTICAS PARA REDUÇÃO DE LARGURA DE BANDA DE MATRIZES", \
+    Notas em Matemática Aplicada, Volume 75, (2014). E-ISSN 2236-5915.
+    Para obter novas matrizes:
+    --------------------------
+    https://sparse.tamu.edu
     """
 
     # Carregando o arquivo de formao Matriz Market filename
     matriz = mmread(file)
-    print("\nApresentando a Matriz:")
-    print("\tDimensao (M): ", matriz.shape)
-    print("\tElementos NONZERO: ", matriz.nnz)
-    print("\tArquivo da Matriz: ", file)
 
     # Convertendo com CSR uma Matriz Esparsa
     G_sparse = csr_matrix(matriz)
 
-    print("\nAplicando a Heuristica REVERSE-CUTHIL-MCKEE")
+    print(" Aplicando a Heuristica REVERSE-CUTHIL-MCKEE")
     t1 = time.time()
     print("\tInicio\t- ", date.datetime.fromtimestamp(t1))
     reverse_cuthill_mckee(G_sparse, symetric)
     t2 = time.time()
     print("\tTermino\t- ", date.datetime.fromtimestamp(t2))
-    print("Tempo de Execucao da Heuristica REVERSE-CUTHIL-MCKEE: ", t2 - t1)
-    print("\n")
+    print(" Tempo de Execucao da Heuristica REVERSE-CUTHIL-MCKEE: ", t2 - t1)
 
+    # Sumarizando o Dataset da Matriz
+    print("\n-------------------------------------------------------------------------------------------------")
+    print(" [SUMARIO] - Apresentando a Matriz")
+    print("-------------------------------------------------------------------------------------------------\n")
+    print(" Dimensao (NxN): \t", matriz.shape)
+    print(" Elementos NONZERO: \t", matriz.nnz)
+    print(" Arquivo da Matriz: \t", file)
 
-if __name__ == '__main__':
-    
-    # Definição dos argumentos de execução
-    parser = argparse.ArgumentParser(description="Tempo de Execucao da Heuristica para Reducao de Largura de Banda")
-    group = parser.add_mutually_exclusive_group()
-    group.add_argument("-v", "--verboso", help="Execucao em tela", action="store_true")
-    group.add_argument("-q", "--silencioso", help="(Default) Execucao em background", action="store_true")
-    parser.add_argument("symetric_mode", type=bool, help=": True (ou False), se a Matriz for simetrica (ou nao)")
-    parser.add_argument("filename", type=str, help=": O nome do arquivo da matriz (extensoes .mtx, .mtz.gz)")
-    args = parser.parse_args()
+    if symetric == None or symetric == False:
+        texto = NONSYMETRIC
+    else:
+        texto = YESSYMETRIC
 
-    if args.silencioso:
-        print("Estao faltando argumentos")
-    elif args.verboso:
-        print("A Reducao de Largura de Banda vai ser aplicada na Matriz (*.mtx) {} com forma SIMETRICA = {}.".format(args.filename, args.symetric_mode))
+    print(" Matriz Simétrica: \t", symetric, texto)
 
-    # Executa a heuristica
-    heuristica_bandwidth(args.filename, args.symetric_mode)
+    return (t2 - t1)
